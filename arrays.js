00f80bb1001381532742e529a1ec07aafe15053d
Gabriela Fijalkowska <gabrielafijalkowska@gmail.com>
2017-09-09 06:22:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,7 +13,7 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   var newArray = [ element, ...array ];
-  return newArray
+  return array
 }
 
 function addElementToEndOfArray(array, element) {
